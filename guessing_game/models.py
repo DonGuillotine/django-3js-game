e739b82db011965ff992c8872f79256477cbe5a5
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-27 19:29:18 +0100

    Added comment for Model

--- guessing_game/models.py
+++ guessing_game/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
 
+# This model will store the randomly chosen number and the number of attempts made by the user.
 class Game(models.Model):
     number = models.IntegerField()
     attempts = models.IntegerField(default=0)
