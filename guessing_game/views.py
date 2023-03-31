690c221381ef323bf08628f03fc2cfce89b1e808
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-31 21:35:06 +0100

    Cleaning Up

--- guessing_game/views.py
+++ guessing_game/views.py
@@ -37,11 +37,7 @@ def game_view(request):
         # Save to database
         game.save()
         # Render to index.html
-        return render(request, 'index.html', {'message': message})
-
-
-
-    
+        return render(request, 'index.html', {'message': message})  
     else:
         game = Game.objects.create(number=random.randint(0, 999))
         return render(request, 'index.html')
